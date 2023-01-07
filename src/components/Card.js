f622c0e719458d98746f8616306cbc1d74bad15c
Diana Kechyants <diana.kechyants@gmail.com>
2023-01-07 22:51:13 +0400

    added material ui/Icons,added onclick on buttons

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,25 +1,46 @@
-import '../App.css';
-
-function Card(props) {
-	const { name, price, src } = props;
-	return (
-		<div className="card">
-			{/* <div className="like">
+import "../App.css";
+import AddIcon from "@mui/icons-material/Add";
+import DoneIcon from "@mui/icons-material/Done";
+function Card({ prod, cart, setCart }) {
+  return (
+    <div className="card">
+      {/* <div className="like">
         {/* <img src="/img/heart.jpg" alt="Heart" width={20} height={20} /> 
       </div> */}
-			<img width={133} height={112} src={src} alt="Sneakers" />
-			<p>{name}</p>
-			<div className="d-flex justify-between">
-				<div className="d-flex ">
-					<span>Price:</span>
-					<b>{price} $</b>
-				</div>
-				<button style={{ cursor: 'pointer' }}>
-					<img width={11} height={11} src="/img/plus2.png" alt="Plus" />
-				</button>
-			</div>
-		</div>
-	);
+      <img width={133} height={112} src={prod.src} alt="Sneakers" />
+      <p>{prod.name}</p>
+      <div className="d-flex justify-between">
+        <div className="d-flex ">
+          <span>Price:</span>
+          <b>{prod.price} $</b>
+        </div>
+        {cart?.includes(prod) ? (
+          <DoneIcon
+            style={{
+              border: "1px solid #f2f2f2 ",
+              color: "white",
+              backgroundColor: "#5ced73 ",
+              cursor: "pointer",
+            }}
+            onClick={() => {
+              setCart(cart.filter((item) => item.id !== prod.id));
+            }}
+          />
+        ) : (
+          <AddIcon
+            style={{
+              border: "1px solid #f2f2f2 ",
+              color: "#6C6C6C",
+              cursor: "pointer",
+            }}
+            onClick={() => {
+              setCart([...cart, prod]);
+            }}
+          />
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Card;
